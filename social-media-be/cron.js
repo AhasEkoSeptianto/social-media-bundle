23f099cc00c4c3aac57be6035f1ed9423f284141
unknown <ahaseko1@gmail.com>
2026-07-27 23:48:37 +0700

    exit cront

--- social-media-be/cron.js
+++ social-media-be/cron.js
@@ -1,13 +1,29 @@
+const { default: mongoose } = require("mongoose");
 const connectDB = require("./src/config/database");
 const postModel = require("./src/models/post.model");
 
 async function testCron() {
-  await connectDB();
-  const post = await postModel.findOneAndUpdate(
-    { _id: "6a5c1f023d5d6bf75e83853b" },
-    { $inc: { sharesCount: 1 } },
-    { new: true },
-  );
+  try {
+    await connectDB();
+
+    const post = await postModel.findOneAndUpdate(
+      { _id: "6a5c1f023d5d6bf75e83853b" },
+      { $inc: { sharesCount: 1 } },
+      { new: true },
+    );
+
+    console.log(post);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await mongoose.connection.close();
+    console.log("MongoDB disconnected");
+  }
 }
 
-testCron();
+testCron()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
